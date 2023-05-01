9f8bb3b27db26a71d77f4d654a7e5390506357e9
jeremylaratro <jlaratro24@gmail.com>
2023-05-01 15:06:13 -0400

    modified:   block.sh

--- block.sh
+++ block.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #enter your desired snort logfile path here (ie. snort.log, snort.alert,etc)
-LOGFILE=/var/log/snort.alert
+LOGFILE=/var/log/snort.alert.fast.1
 # use grep to grab any priority 1 alerts, then extract both IPv4 and IPv6 and add them to the ufw deny list
 cat /var/log/snort/snort.alert.fast.1 | grep 'Priority: 1' | grep  -Eo -- '->'.[0-9].+.+[0-9] | tr -d -- '->' > priority1.txt
 sed 's/....$//' < priority1.txt > priority1_procd.txt
